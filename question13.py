420fd6639662f1333b8856b7f74d7438c0002c1b
yarynazhurkevych <144909755+yarynazhurkevych@users.noreply.github.com>
2023-10-03 11:03:15 +0300

    Delete question13.py

--- question13.py
+++
@@ -1,7 +0,0 @@
-length_in_cm = float(input("Enter a length in centimeters: "))
-
-if length_in_cm < 0:
-    print("The entry is invalid")
-else:
-    length_in_inches = length_in_cm / 2.54
-    print(f"Length in inches: {length_in_inches}")
